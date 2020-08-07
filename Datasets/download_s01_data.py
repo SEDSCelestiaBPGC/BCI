cf8511058a7412ad67f638ce8c8e8b773aa0b495
Yash-10 <68844397+Yash-10@users.noreply.github.com>
2020-08-07 15:59:43 +0530

    Update download_s01_data.py

--- Datasets/download_s01_data.py
+++ Datasets/download_s01_data.py
@@ -8,69 +8,63 @@ import wget
 import pandas as pd
 import numpy as np
 import os
-
-# we're downloading only subject_01 data for now, but the url can be altered to download any of the 52 subjects' data
-urls = []
-nums = 53
-for i in range(nums):
-  if i in range(1, 10):
-    number = '{}'.format(i)
-    urls.append('ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100295/mat_data/')
-    wget.download(urls[i], out='s'+number.zfill(2)+'.mat')  
-  else:
-    urls.append('ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100295/mat_data/')
-    wget.download(urls[i], out='s{}'.format(i)+'.mat')
-
-# ---- Following code is for reading the mat file and extracting that data we need: -------
-
 import scipy.io
 
-num_subjects = 53
-left = []
-right = []
-event_markers = []
+def download_mat(subjects):
+  urls = []
+  nums = 53
+  left = []
+  right = []
+  event_markers = []
+  for i in range(len(subjects)):
+    sub = subjects[i]
+    urls.append('ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100295/mat_data/' + sub)
+    #print(urls)
+    wget.download(urls[i], os.path.join('/content', sub))
 
-for i in range(1, num_subjects):
-  if i in range(1, 10):
-    num = '{}'.format(i)
-    subject_i = scipy.io.loadmat(os.path.join('/content', 's'+num.zfill(2)+'.mat'))
-  else:
-    subject_i = scipy.loadmat(os.path.join('/content', 's{}'.format(i)+'.mat'))
-  # how subject.mat files are structured is desccribed here: 
-  # https://academic.oup.com/gigascience/article/6/7/gix034/3796323
-  left.append(subject_i['eeg']['imagery_left'][0][0][:64])
-  right.append(subject_i['eeg']['imagery_right'][0][0][:64])
-  event_markers.append(subject_i['eeg']['imagery_event'][0][0][0])
+  # ---- Following code is for reading the mat file and extracting that data we need: -------
+    #print('Subject {} mat file downloaded! Starting to load the .npy files'.format(sub[1:3]))
+    
+    subject_i = scipy.io.loadmat('/content/' + sub)
+    # how subject.mat files are structured is desccribed here: 
+    # https://academic.oup.com/gigascience/article/6/7/gix034/3796323
 
-  # print(left.shape, event_markers.shape) # (64, 358400) (358400,)
+    left.append(subject_i['eeg']['imagery_left'][0][0][:64])
+    right.append(subject_i['eeg']['imagery_right'][0][0][:64])
+    event_markers.append(subject_i['eeg']['imagery_event'][0][0][0])
 
-  df_left = pd.DataFrame(left[i]).T.rename(columns = lambda x: 'left'+str(x))
-  df_right = pd.DataFrame(right[i]).T.rename(columns = lambda x: 'right'+str(x))
-  markers = pd.Series(event_markers[i])
+    # print(left.shape, event_markers.shape) # (64, 358400) (358400,)
 
-df = df_left.join(df_right)
-df['mark'] = markers
+    df_left = pd.DataFrame(left[i]).T.rename(columns = lambda x: 'left'+str(x))
+    df_right = pd.DataFrame(right[i]).T.rename(columns = lambda x: 'right'+str(x))
+    markers = pd.Series(event_markers[i])
 
-ind = list(df[df['mark'] == 1].index)
-ind = ind + [358399]
-intervals = list(zip(ind[:-1], ind[1:]))
+    df = df_left.join(df_right)
+    df['mark'] = markers
 
-dfset_left = []
-dfset_right = []
-for start, end in intervals[:-1]:
-    dfset_left.append(df.loc[start:end-1, 'left0':'left63'])
-    dfset_right.append(df.loc[start:end-1, 'right0':'right63'])
+    ind = list(df[df['mark'] == 1].index)
+    ind = ind + [358399]
+    intervals = list(zip(ind[:-1], ind[1:]))
 
-bci_left = []
-for df in dfset_left:
-    bci_left.append(df.values.T)
-bci_left = np.array(bci_left)
-np.save('bci_imagery_left.npy', bci_left)
+    dfset_left = []
+    dfset_right = []
+    for start, end in intervals[:-1]:
+        dfset_left.append(df.loc[start:end-1, 'left0':'left63'])
+        dfset_right.append(df.loc[start:end-1, 'right0':'right63'])
 
-bci_right = []
-for df in dfset_right:
-    bci_right.append(df.values.T)
-bci_right = np.array(bci_right)
-np.save('bci_imagery_right.npy', bci_right)
+    bci_left = []
+    for df in dfset_left:
+        bci_left.append(df.values.T)
+    bci_left = np.array(bci_left)
+    np.save('bci_imagery_left_sub{}.npy'.format(sub[1:3]), bci_left)
 
-# print(bci_left.shape, bci_right.shape) # (99, 64, 3584) (99, 64, 3584)
+    bci_right = []
+    for df in dfset_right:
+        bci_right.append(df.values.T)
+    bci_right = np.array(bci_right)
+    np.save('bci_imagery_right_sub{}.npy'.format(sub[1:3]), bci_right)
+
+    # print(bci_left.shape, bci_right.shape) # (99, 64, 3584) (99, 64, 3584)
+
+# Example testing   
+download_mat(['s01.mat', 's02.mat', 's03.mat'])
